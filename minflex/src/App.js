354cdf3a686e3443deb1c0e343519937cd27e7de
Minna <minna1025@gmail.com>
2022-01-23 14:32:26 +0900

    deploy 404 erro edit

--- minflex/src/App.js
+++ minflex/src/App.js
@@ -4,12 +4,14 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={Detail}></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={Home}></Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
